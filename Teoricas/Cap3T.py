0338578f468ee49cfe4be10258c208adc4855ea0
jppf22 <jppf22@live.com>
2022-09-28 10:02:05 +0100

    cap3

---
+++ Teoricas/Cap3T.py
@@ -0,0 +1,26 @@
+
+
+''' Exemplos Funções
+
+def soma(x,y):
+    return x+y
+
+print(soma(7,5)) 
+
+'''
+
+''' Exemplo com recursão (NÃO É SUPOSTO USAR ATÉ SER DADO)
+def soma_ar(n):
+    if(n == 1):
+        return n
+    else:
+        return n + soma_ar(n-1)
+
+print(soma_ar(10))
+'''
+
+#''' Igual ao anterior mas de acordo com matéria
+
+
+
+#'''
