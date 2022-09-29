73c50fdd90356b6481c40b17a29bcfc4c472d9a0
jppf22 <jppf22@live.com>
2022-09-29 09:56:47 +0100

    Aula de Erros/Excecoes

--- Teoricas/Cap3T.py
+++ Teoricas/Cap3T.py
@@ -22,5 +22,18 @@ print(soma_ar(10))
 #''' Igual ao anterior mas de acordo com matéria
 
 
+''' Exemplos de uso de erro/excepções
 
-#'''
+x = eval(input("Introduza um número: "))
+
+def inverte(n):
+    if(type(n) == int or type(n) == float) and n != 0:
+        return 1/n
+    elif n == 0:
+        raise ZeroDivisionError("inverte: divisão por zero")
+    else:
+        raise ValueError("inverte: argumento inválido") #por enquanto quando levantarmos um erro será sempre um catastrófico (termina o programa)        
+
+print("O inverso do número é ", inverte(x))
+
+'''
