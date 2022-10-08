cd5edfbd51c0580acec0e83a0a71a448a751b1df
João Fernandes <jppf22@live.com>
2022-10-08 17:57:26 +0100

    final_lista

--- Práticas/Cap3Lista.py
+++ Práticas/Cap3Lista.py
@@ -110,8 +110,97 @@ print(duplicar(100,0.03))
 '''
 #---------------------------------------------------------------------------
 
-#''' Exercicio 8
+''' Exercicio 8 
 
-def serie_geom(r,n)
+def serie_geom(r,n):
+    if(n < 0):
+        raise ValueError("serie_geom: argumento incorrecto")
+    
+    soma=0
+    i=0
+    while n >= 0:
+        soma += r**i
+        n -= 1
+        i+=1
+    
+    return soma
 
-#'''
+print(serie_geom(2,4))
+print(serie_geom(100,0))
+#print(serie_geom(100,-1))
+
+'''
+
+#------------------------------------------------------
+
+''' Exercicio 9 (Verificado no google Calendars)
+def converter_mes(mes):
+    # Converte o mes normal para o que é utilizado na formula
+    
+    if(mes < 3): #se for janeiro ou fevereiro
+        mes = mes + 12
+    
+    return mes
+
+def chao(x):
+    #Converte x no maior número inteiro menor ou igual que x
+    
+    return int(x) #não pode ser round pois ultrapassaria x
+
+def dia_da_semana(dia,mes,ano):
+    mes = converter_mes(mes)
+
+    if(mes > 12):
+        ano -= 1
+
+    ano_seculo = ano % 100
+    seculo = ano // 100
+
+    #Calcula o resultado da formula
+    h = (dia + chao((13*(mes+1))/5) + ano_seculo \
+    + chao(ano_seculo/4) + chao(seculo/4) - (2*seculo)) \
+    % 7
+
+    dias_semana = ("sabado","domingo","segunda","terça","quarta","quinta","sexta")
+    return dias_semana[h]
+
+#print(dia_da_semana(18,1,2014))
+#print(dia_da_semana(20,9,2017))
+#print(dia_da_semana(8,10,2022))
+'''
+
+# -----------------------------------------------
+
+''' Exercicio 10
+
+def inverter(n):
+    res = 0
+    while n != 0:
+        dig = n%10
+        res = res*10 + dig
+        n = n // 10
+    
+    return res
+
+def misterio(n):
+    #Error checking
+    if(type(n) != int):
+        raise TypeError("misterio: tipo de argumento invalido")
+    if(not(100 <= n < 1000)):
+        raise ValueError("misterio: inteiro com mais ou menos que 3 digitos")
+
+    primeiro_dig = n // 100
+    ultimo_dig = n % 10
+    if(abs(primeiro_dig-ultimo_dig)>1):
+        n_i = inverter(n)
+        n_s = abs(n - n_i)
+        n_si = inverter(n_s)
+        return n_s + n_si
+    else:
+        return "Condições não verificadas"
+
+print(misterio(246))
+print(misterio(131))
+#print(misterio(1001))
+
+'''
