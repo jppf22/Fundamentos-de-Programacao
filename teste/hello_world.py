358c3d4020a8f140308f072fc6f738abb559f5e4
João Fernandes <jppf22@live.com>
2022-10-26 10:51:54 +0100

    a

--- teste/hello_world.py
+++ teste/hello_world.py
@@ -1,7 +1,11 @@
-
-s = input("Introduza um nome: ")
-n = input("Introduza uma seccao do dia: ")
-
-print(f"Hello {s}, how was your {n}?")
-
-
+def mover(n, ori, dest, aux):
+    def movimentar(ori, dest):
+        print(ori, '-->', dest)
+    if n == 1:
+        movimentar(ori, dest)
+    else:
+        mover(n-1, ori, aux, dest)
+        movimentar(ori, dest)
+        mover(n-1, aux, dest, ori)
+         
+mover(3, 'E', 'D', 'C')
