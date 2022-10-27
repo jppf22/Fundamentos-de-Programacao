9da27cd2bdf5ee437a3946e77db8f03fe2bd43d5
jppf22 <jppf22@live.com>
2022-10-27 11:47:50 +0100

    nova ficha de recursao

---
+++ Práticas/Cap7Listas.py
@@ -0,0 +1,142 @@
+# Exericico 1 ---------------------------------------
+
+# a) Somatório que começa em i = 0 e vai até n de x * i
+
+# b)
+'''
+misterior(2,3)
+return 2*3 + misterio(2,2)
+    return 2*2 + misterio(2,1)
+        return 2*1 + misterio(2,0)
+            return 0
+        return 2+0
+    return 4+2
+return 6+6
+
+output: 12
+'''
+
+#c) Recursão de operações adiadas, estamos sempre a adiar a obtenção do resultado até chegar ao caso terminal
+
+#d)
+
+'''
+def misterio(x,n):
+    def misterio_aux(x,n,acc):
+        if(n==0):
+            return acc 
+        else:
+            return misterio_aux(x,n-1,x*n + acc) #não há operações adiadas, são determinados logo os resultados
+    return misterio_aux(x,n,0)
+
+#print(misterio(2,3)) #2*3 + 2*2 + 2*1 + 2*0 = 12
+
+'''
+
+# Exericico 2 -------------------------------------
+'''
+
+#a)
+
+def quadrado_rec_adiadas(n):
+    if(n == 0):
+        return 0
+    else:
+        return (n+n - 1) + quadrado_rec_adiadas(n-1)
+
+print(quadrado_rec_adiadas(6))
+
+#b)
+
+def quadrado_rec_cauda(n):
+    def quadrado_rec_cauda_aux(n, acc):
+        if(n == 0):
+            return acc
+        else:
+            return quadrado_rec_cauda_aux(n-1,n+n-1+acc)
+    return quadrado_rec_cauda_aux(n,0)
+
+print(quadrado_rec_cauda(6))
+
+#c)
+
+def quadrado_iteracao(n):
+    res = 0
+    while n > 0:
+        res += n+n-1
+        n -= 1
+    return res
+
+print(quadrado_iteracao(6))
+'''
+
+# Exercicio 3 -------------------------
+
+'''
+#a)
+def numero_digitos_rec_adiada(n):
+    if(type(n) != int or n <= 0):
+        raise ValueError("numero_digitos_rec_adiada: argumento invalido")
+    
+    if(n < 10):
+        return 1
+    else:
+        return numero_digitos_rec_adiada(n//10) + 1
+
+print(numero_digitos_rec_adiada(5153534))
+
+#b)
+def numero_digitos_rec_cauda(n):
+    def numero_digitos_rec_cauda_aux(n,acc):
+        if(type(n) != int or n <= 0):
+            raise ValueError("numero_digitos_rec_adiada: argumento invalido")
+
+        if(n < 10):
+            return acc+1
+        else:
+            return numero_digitos_rec_cauda_aux(n//10,acc+1)
+    return numero_digitos_rec_cauda_aux(n,0)
+
+print(numero_digitos_rec_cauda(5153534))
+
+#c) 
+
+'''
+from re import T
+
+
+def numero_digitos_iterativo(n):
+    if(type(n) != int or n <= 0):
+        raise ValueError("numero_digitos_rec_adiada: argumento invalido")
+    
+    occ = 0
+    while n > 0:
+        occ += 1
+        n = n // 10
+    
+    return occ
+
+print(numero_digitos_iterativo(5153534))
+
+# Exericio 4--------------------------------------------------------
+
+def eh_capicua(n):
+    def eh_capicua_aux(n):
+
+        if(n == 0 or 0 < n < 10):
+            return True
+
+        dig_f = n%10
+        dig_i = n//(10**(numero_digitos_iterativo(n)-1))
+
+        if(dig_f != dig_i):
+            return False
+        else:
+            n = n//10
+            n = n - dig_i*(10**(numero_digitos_iterativo(n)-1))
+            return eh_capicua_aux(n)
+    return eh_capicua_aux(n)
+
+#print(eh_capicua(10101)) #se não alteressamos o numero original e passasemos parametros que são o n da esquerda e da diretia seria possivel
+#print(eh_capicua(1221))
+#print(eh_capicua(12121))
